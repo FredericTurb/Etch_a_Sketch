e688b7353b88e491f95ffcdf85671759b3f09e79
FredericTurb <185390564+FredericTurb@users.noreply.github.com>
2024-10-25 16:30:32 +0200

    commit of project working

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
+let userChoice = 16;
+
 createDiv();
 
 
 const constReset = document.getElementById("reset");
 constReset.addEventListener("click", myDelete);
 
-function myDelete()
-{
+const constSize = document.getElementById("select");
+constSize.addEventListener("click", howMany);
+
+function myDelete(userChoice)
+{   
     const toDelete = document.getElementById("maindiv");
     while (toDelete.firstChild)
     {
         toDelete.firstChild.remove();
     }
-    createDiv();
-}
+    createDiv(userChoice);}
 
-function createDiv()
-{
+function createDiv(){
     const myDiv = document.getElementById("maindiv");
     myDiv.style.display = "grid";
-    myDiv.style.gridTemplateColumns = "repeat(16, 1fr)";
-    myDiv.style.gridTemplateRows = "repeat(16, 1fr)";
+    myDiv.style.gridTemplateColumns = `repeat(${userChoice}, 1fr)`;
+    myDiv.style.gridTemplateRows = `repeat(${userChoice}, 1fr)`;
     myDiv.style.gap = "2px";
 
-    for (let i = 0; i < 256; i++){
+    for (let i = 0; i < userChoice * userChoice; i++){
         const contentDiv = document.createElement("div");
         contentDiv.classList.add("new")
         myDiv.appendChild(contentDiv);
         contentDiv.addEventListener("mouseover",() => {
             contentDiv.style.backgroundColor = 'black';
         })
+    }}
+
+function howMany(){
+    regex = /^\d*$/;
+    userChoice = prompt("How many rows would you like ? (Max 90, Defaut 16)");
+    if (userChoice > 90 || userChoice === null || userChoice === "" || !regex.test(userChoice))
+    {
+        howMany();
     }
-}
+    else{
+    myDelete(userChoice);}}
